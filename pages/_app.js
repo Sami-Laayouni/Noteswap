26b3f8f64511c72304ca9aaa329c9197d56b38c6
Sami-Laayouni <therealsamilaayouni@gmail.com>
2023-06-04 15:22:46 +0100

    ci: intial-commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,22 @@ import "../styles/globals.css";
 /**
  * Custom App component for Next.js.
  *
- * @param {AppProps} props - The props for the App component.
- * @returns {JSX.Element} The rendered component.
+ * @param {Object} props - The props for the App component.
+ * @param {React.ComponentType} props.Component - The page component to render.
+ * @param {Object} props.pageProps - The initial props for the page component.
+ * @return {JSX.Element} The rendered app.
+ *
+ * @throws {TypeError} If `Component` is not a valid React component.
+ * @example
+ * // Example usage of NoteSwap component
+ * <MyApp Component={Home} pageProps={{ initialData: data }} />
+ *
+ * @date 6/4/2023 - 3:01:24 PM
+ * @author Sami Laayouni
+ * @license MIT
  */
-
-function MyApp({ Component, pageProps }) {
+function NoteSwap({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default NoteSwap;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 /**
- * Home page.
+ * Home Page
  *
- * @returns {JSX.Element} The rendered component.
+ * @return {JSX.Element} The rendered page.
+ * @date 6/4/2023 - 3:02:38 PM
+ * @author Sami Laayouni
+ * @license MIT
  */
-
 export default function Home() {
   return <></>;
 }
