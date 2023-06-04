adf27f6fc843397cdba81be47d528c9509dfe50b
Sami-Laayouni <therealsamilaayouni@gmail.com>
2023-06-04 15:21:21 +0100

    ci: intial-commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+/**
+ * Custom App component for Next.js.
+ *
+ * @param {AppProps} props - The props for the App component.
+ * @returns {JSX.Element} The rendered component.
+ */
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
