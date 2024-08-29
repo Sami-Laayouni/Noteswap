78c5897aae685ddecf08ba804937dcd1a9e11eb0
Sami Laayouni <sam.laayouni@aui.ma>
2024-08-29 15:30:45 +0100

    ci: fix

--- components/Modals/TicketModal/index.js
+++ components/Modals/TicketModal/index.js
@@ -352,7 +352,6 @@ export default function TicketModal() {
                     "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
                     currency: "USD",
                     intent: "capture",
-                    country: "MA",
                   }}
                 >
                   <PayPalButtons
