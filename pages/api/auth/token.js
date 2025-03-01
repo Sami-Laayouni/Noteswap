cab51675e1c8acc9848bcd35942baf29cb8cde47
Sami Laayouni <sam.laayouni@aui.ma>
2025-03-01 22:06:06 +0000

    ci: new

--- pages/api/auth/token.js
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-
-export async function GET(req, res) {
-  const getCookies = cookies();
-  const nextAuthSession =
-    getCookies.get("next-auth.session-token")?.value || "";
-
-  return NextResponse.json(nextAuthSession);
-}
